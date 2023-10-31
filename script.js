11af9f1c7402610583216726d3229917ca0aba70
bmouser <pmouser@ivytech.edu>
2023-10-30 21:19:49 -0400

    init commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Your solution goes here 
+let tvShow = prompt();
+
+if (tvShow === null) {
+   console.log("You did not enter a TV show.");
+}
+else {
+   console.log(tvShow + " dididid   is your favorite TV show!");
+}
